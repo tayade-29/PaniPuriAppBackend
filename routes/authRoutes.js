bcd26b0d88b4e64aed66b51fcc252e3fb5b36c43
tayade-29 <tayadepranjal2903@gmail.com>
2025-11-10 17:17:15 +0530

    Login Route created

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -70,4 +70,36 @@ res.status(201).json({
     }
 
 });
+
+router.post('/login', async(req, res) =>{
+   try{
+    const { email, password } = req.body;
+    const user =await User.findOne({ email: email.toLowerCase() });
+    if(!user){
+        return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    const isMatch =await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.status(400).json({ message: 'Invalid email or password' });
+    }
+     const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+     res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.fullName,
+        email: user.email,
+      },
+    });
+   }
+   catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+})
+
 module.exports = router;
